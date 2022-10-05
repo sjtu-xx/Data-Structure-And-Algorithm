78354f51c0a128658c759aceecb4af28de920275
xuexuan <1099313374@qq.com>
2022-10-05 17:41:00 +0800

    约瑟夫问题

--- linkedlist/singleCircleLinkedList/JosephuProblem.java
+++ linkedlist/singleCircleLinkedList/JosephuProblem.java
@@ -1,4 +1,42 @@
 package linkedlist.singleCircleLinkedList;
 
+import linkedlist.Node;
+
+import java.awt.print.Printable;
+
 public class JosephuProblem {
+    public static void main(String[] args) {
+        int start = 1, kill_count = 2, total = 5;
+        SingleCircleLinkedList childrens = new SingleCircleLinkedList();
+        childrens.add(total);
+        Node first = childrens.first;
+        Node helper = first;
+        while (true) {
+            if (helper.next == first) {
+                break;
+            }
+            helper = helper.next;
+        }
+
+        for (int i = 0; i < start - 1; i++) {
+            first = first.next;
+            helper = helper.next;
+        }
+
+        while (true) {
+            if (first == first.next) {
+                System.out.println(first.value + 1);
+                break;
+            }
+            for (int i = 0; i < kill_count - 1; i++) {
+                first = first.next;
+                helper = helper.next;
+            }
+            System.out.println(first.value + 1);
+            first = first.next;
+            helper.next = first;
+        }
+
+    }
+
 }
--- linkedlist/singleCircleLinkedList/SingleCircleLinkedList.java
+++ linkedlist/singleCircleLinkedList/SingleCircleLinkedList.java
@@ -1,4 +1,44 @@
 package linkedlist.singleCircleLinkedList;
 
+import linkedlist.Node;
+
 public class SingleCircleLinkedList {
+    public Node first;
+
+    public SingleCircleLinkedList() {
+    }
+
+     public void add(int number) {
+        if (number < 1) return;
+        Node cur = null;
+        for (int i = 0; i < number; i++) {
+            if (i == 0) {
+                first = new Node(i);
+                first.next = first;
+                cur = first;
+            } else {
+                Node newNode = new Node(i);
+                newNode.next = first;
+                cur.next = newNode;
+                cur = newNode;
+            }
+        }
+    }
+
+    private void show() {
+        Node cur = first;
+        while (true) {
+            if (cur.next == first) {
+                break;
+            }
+            System.out.println(cur.value);
+            cur = cur.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        SingleCircleLinkedList singleCircleLinkedList = new SingleCircleLinkedList();
+        singleCircleLinkedList.add(10);
+        singleCircleLinkedList.show();
+    }
 }
