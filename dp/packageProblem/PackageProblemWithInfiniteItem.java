2134c3331d11bb0525e555b88fd50d104f7e7792
xuexuan <1099313374@qq.com>
2022-10-22 10:42:16 +0800

    动态规划

--- dp/packageProblem/PackageProblemWithInfiniteItem.java
+++ dp/packageProblem/PackageProblemWithInfiniteItem.java
@@ -1,7 +1,5 @@
 package dp.packageProblem;
 
-import java.util.Arrays;
-
 public class PackageProblemWithInfiniteItem {
     public static void main(String[] args) {
         System.out.println(getMaxValue());
